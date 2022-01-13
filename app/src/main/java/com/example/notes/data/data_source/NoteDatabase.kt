80cfcea88e24932848f23b89e184502a43ee4b5a
Walison Ferreira <walisonferreiraa@gmail.com>
2022-01-12 21:09:24 -0300

    dagger-hilt configuration

--- app/src/main/java/com/example/notes/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/example/notes/data/data_source/NoteDatabase.kt
@@ -11,4 +11,8 @@ import com.example.notes.domain.model.Note
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
