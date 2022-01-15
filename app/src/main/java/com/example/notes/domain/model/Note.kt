10e62a65865677f8cafc6df641efcf9ea35cdbe8
Walison Ferreira <walisonferreiraa@gmail.com>
2022-01-15 16:53:31 -0300

    created NotesScreen to show list of notes

--- app/src/main/java/com/example/notes/domain/model/Note.kt
+++ app/src/main/java/com/example/notes/domain/model/Note.kt
@@ -1,8 +1,10 @@
 package com.example.notes.domain.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.notes.ui.theme.*
 
+@Entity
 data class Note(
     val title: String,
     val content: String,
